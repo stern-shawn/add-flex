482a00a477dc2a326d0f633e5cecfb6454cb7741
Shawn Stern <sstern@twilio.com>
2022-12-07 11:00:39 -0500

    feat: attempt integration of Flex w/ CRA and react router dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as Flex from '@twilio/flex-ui';
+import { Router, Route, RouteComponentProps, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const history = createBrowserHistory();
+
+// @ts-ignore
+Flex.Manager.create({
+  router: {
+    type: 'browser',
+  },
+}).then((manager) => {
+  root.render(
+    <Router history={history}>
+      <Switch>
+        <Route
+          path="/"
+          exact
+          component={() => (
+            // @ts-ignore
+            <Flex.ContextProvider manager={manager}>
+              <Flex.RootContainer />
+            </Flex.ContextProvider>
+          )}
+        />
+        <Route
+          path="/hi"
+          component={() => {
+            setTimeout(
+              () => Flex.Actions.invokeAction('HistoryPush', '/'),
+              5000
+            );
+            return <div>Hi! I will redirect to Flex in 5 seconds flat!</div>;
+          }}
+        />
+      </Switch>
+    </Router>
+  );
+});
